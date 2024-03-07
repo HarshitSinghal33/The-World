e773e8cfabeaa74d24b9ea53766c718ced0fc6e2
HarshitSinghal33 <124229061+HarshitSinghal33@users.noreply.github.com>
2024-03-07 10:05:19 +0530

    Fix : Routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <BrowserRouter>
       <AppContextProvider>
         <Routes>
-          <Route path='/' element={<Countries/>}/>
-          <Route path='/country/:code' element={<Country/>} />
+          <Route path='/the-world/' element={<Countries/>}/>
+          <Route path='/the-world/country/:code' element={<Country/>} />
           <Route path='*' element={<h1 className='flex h-screen justify-center items-center'>Page Not Found</h1>}/>
         </Routes>
       </AppContextProvider>
