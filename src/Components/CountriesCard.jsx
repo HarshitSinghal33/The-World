3fe3a143268401c1b30d0b848ed4caeaafccb284
HarshitSinghal33 <124229061+HarshitSinghal33@users.noreply.github.com>
2024-03-07 10:49:12 +0530

    Fix : width and height for responsiveness

--- src/Components/CountriesCard.jsx
+++ src/Components/CountriesCard.jsx
@@ -7,9 +7,19 @@ export default function CountriesCard({ country }) {
     return (
         <Link to={`/the-world/country/${cca2}`} className={`${darkMode ? 'dark' : ''} hover:scale-105 transition-all ease-in duration-100`}>
             <div className='px-6 py-3 cursor-pointer rounded-lg text-center shadow-light dark:text-white  dark:shadow-dark'>
-                <img src={country.flags.svg} alt={`${country.name.common} img`} className='block w-64 h-36 max-sm:w-48 max-sm:h-28 rounded-lg shadow-lg shadow-neutral-400 dark:shadow-dark object-cover' />
+                <img src={country.flags.svg} alt={`${country.name.common} img`} className='block w-64 h-36 max-sm:w-32 max-sm:h-20 rounded-lg shadow-lg shadow-neutral-400 dark:shadow-dark object-cover' />
                 <h1 className='text-xl font-medium mt-0.5 '>{country.name.common.length <= 18 ? country.name.common : `${country.name.common.slice(0, 18)}...`}</h1>
             </div>
         </Link>
     )
 }
+    // (max-width: 960px) and (min-width: 450px){
+    //     .Cards img {
+    //         width: 210px;
+    //         height: 120px;
+    //     }
+    // (max-width: 450px){
+    //     .Cards img {
+    //         width: 135px;
+    //         height: 75px;
+    //     }
