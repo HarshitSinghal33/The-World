bf65183a3fd0e8c45d4e1dea98d9282bee75193c
HarshitSinghal33 <124229061+HarshitSinghal33@users.noreply.github.com>
2024-05-27 12:40:55 +0530

    update for deployment

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({plugins: [
+  require('tailwindcss')(),
+  require('autoprefixer')()
+]})
+// export default {
+//   plugins: {
+//     tailwindcss,
+//     autoprefixer,
+//   },
+// }
